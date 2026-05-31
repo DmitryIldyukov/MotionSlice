2e37ffc1f01f0441fa879e40eff5ac1918736860
Дмитрий Ильдюков <dmitry.ildyukov@gmail.com>
2026-05-31 20:46:37 +0300

    Добавил сущности User, ExternalLogin и RefreshToken

---
+++ src/Services/Auth/Auth.Domain/Entities/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace Auth.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; init; } = Guid.CreateVersion7();
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
